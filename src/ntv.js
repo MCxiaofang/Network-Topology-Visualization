47d193f1a996cb7fe8d92e2c45e2683821de9253
Yunlei Zhao <809192837@qq.com>
2024-06-30 20:21:04 +0800

    success pack by UMD, test empty function

--- src/ntv.js
+++ src/ntv.js
@@ -1,19 +1,31 @@
-// src/ntv.js
-import { setOption } from './setOption';
-
-const ntv = {
-  init: function(dom) {
-    console.log("初始化实例");
-    var instance = {
-      dom: dom,
-      setOption: function(option) {
-        console.log("设置选项", option);
-        setOption(this, option);
-      }
+(function(global, factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+      console.log("CommonJS环境");
+      module.exports = factory(require('d3'), require('./setOption'));
+    } else if (typeof define === "function" && define.amd) {
+      console.log("AMD环境");
+      define(['d3', './setOption'], factory);
+    } else {
+      console.log("浏览器环境");
+      global.ntv = factory(global.d3, global.setOption);
+      console.log(global.ntv);  // 添加调试信息
+    }
+  }(typeof window !== "undefined" ? window : this, function(d3, setOption) {
+    var ntv = {};
+  
+    ntv.init = function(dom) {
+      console.log("初始化实例");
+      var instance = {
+        dom: dom,
+        setOption: function(option) {
+          console.log("设置选项", option);
+          setOption(this, option);
+        }
+      };
+  
+      return instance;
     };
-
-    return instance;
-  }
-};
-
-export default ntv;
+  
+    return ntv;
+  }));
+  
--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,9 @@ module.exports = {
   output: {
     filename: 'ntv.bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    library: {
-      type: 'module' // 指定导出为 ES6 模块
-    },
-    environment: {
-      module: true // 确保输出的代码使用 ES6 模块语法
-    }
-  },
-  experiments: {
-    outputModule: true // 启用模块输出
+    library: 'ntv',
+    libraryTarget: 'umd',
+    globalObject: 'this'
   },
   module: {
     rules: [
@@ -29,5 +23,8 @@ module.exports = {
         }
       }
     ]
+  },
+  externals: {
+    d3: 'd3'
   }
 };
